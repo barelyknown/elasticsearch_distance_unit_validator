8240bd5e0a8be8348ef6aeec3254da90b0618bf0
Sean Devine <barelyknown@icloud.com>
2015-02-04 19:12:46 -0500

    Add tests.

---
+++ spec/lib/elasticsearch_distance_units_validator_spec.rb
@@ -0,0 +1,78 @@
+RSpec.describe TestModel do
+  let :valid_units do
+    ElasticsearchDistanceUnitsValidator::VALID_UNITS
+  end
+
+  let :invalid_units do
+    %w(hands smidges paces)
+  end
+
+  let :valid_numbers do
+    (1..10).to_a
+  end
+
+  let :invalid_numbers do
+    (-1..0).to_a
+  end
+
+  let :invalid_values do
+    [
+      valid_numbers[0].to_s + " " + valid_units[0].to_s, # space in the middle
+      "a" + valid_numbers[0].to_s + valid_units[0].to_s, # starts with a non digit
+      valid_numbers[0].to_s + valid_units[0].to_s + " ", # ends with a space
+      valid_numbers[0].to_s + valid_units[0].to_s + 1.to_s, #ends with a number
+    ]
+  end
+
+  def validate_error_for(subject, number, unit, *messages)
+    subject.distance = number.to_s + unit.to_s
+    subject.valid?
+    messages.each do |message|
+      expect(subject.errors[:distance]).to include Regexp.new(message)
+    end
+  end
+
+  context "when the general format is invalid" do
+    it "distance is invalid and has the correct error message" do
+      invalid_values.each do |value|
+        subject.distance = value
+        subject.valid?
+        expect(subject.errors[:distance]).to include Regexp.new(ElasticsearchDistanceUnitsValidator::INVALID_GENERAL_FORMAT_MESSAGE)
+      end
+    end
+  end
+
+  context "when the number is valid" do
+    context "when the units is valid" do
+      it "distance is valid" do
+        valid_numbers.product(valid_units).each do |number, unit|
+          subject.distance = number.to_s + unit.to_s
+          expect(subject).to be_valid
+        end
+      end
+    end
+    context "when the units are invalid" do
+      it "distance is invalid and has the correct error message" do
+        valid_numbers.product(invalid_units).each do |number, unit|
+          validate_error_for(subject, number, unit, ElasticsearchDistanceUnitsValidator::INVALID_UNIT_MESSAGE)
+        end
+      end
+    end
+  end
+  context "when the number is invalid" do
+    context "when the units is valid" do
+      it "distance is invalid and has the correct error message" do
+        invalid_numbers.product(valid_units).each do |number, unit|
+          validate_error_for(subject, number, unit, ElasticsearchDistanceUnitsValidator::INVALID_NUMBER_MESSAGE)
+        end
+      end
+    end
+    context "when the units is invalid" do
+      it "distance is invalid and has the correct error message" do
+        invalid_numbers.product(invalid_units).each do |number, unit|
+          validate_error_for(subject, number, unit, ElasticsearchDistanceUnitsValidator::INVALID_UNIT_MESSAGE, ElasticsearchDistanceUnitsValidator::INVALID_NUMBER_MESSAGE)
+        end
+      end
+    end
+  end
+end
---
+++ spec/support/test_model.rb
@@ -0,0 +1,9 @@
+require "active_model"
+
+class TestModel
+  include ActiveModel::Validations
+
+  attr_accessor :distance
+
+  validates :distance, elasticsearch_distance_units: true
+end
